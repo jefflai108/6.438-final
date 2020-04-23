48f27a179be5d79e4a08fee409df58fd0de1d577
Daikon <b03901056@ntu.edu.tw>
2020-04-23 20:32:55 +0800

    upd

--- ours/draft.py
+++ ours/draft.py
@@ -6,18 +6,29 @@ import matplotlib.pyplot as plt
 
 
 # Assume batch size = 1
+# Problems:
+# 1. Next word prob != Classification prob
+# 2. Negation is not considered (not good v.s. not bad)
+# 3. Top k words don't contain valid words (The chicken tasts + negative)
 
 
 MODEL = 'gpt2-medium'
 DEV = 'cuda'
-COND = 'negative politics'
-COND = 'positive science'
-COND = 'negative'
-TOP_K = 30
-PREFIX = 'To conclude'
-PREFIX = 'The potato'
-LENGTH = 19
+TOP_K = 100
+LENGTH = 20
 WEIGHT = 1
+REDUCE = 'mean'
+
+COND = 'positive science'
+COND = 'negative science'
+COND = 'negative politics'
+COND = 'positive politics'
+COND = 'positive'
+COND = 'negative'
+
+PREFIX = 'The potato'
+PREFIX = 'To conclude'
+PREFIX = 'The chicken tastes'
 
 
 def top_k_filtering(logits, top_k=1, filter_value=-float("Inf"), min_tokens_to_keep=1):
@@ -33,8 +44,10 @@ def conditioning(logprobs, cond_ids, model, input_ids, ids_to_retain):
     input_ids = torch.cat([input_ids, ids_to_retain.unsqueeze(1)], dim=-1)
     next_logits = model(input_ids)[0][:, -1]
     next_logprobs = F.log_softmax(next_logits, dim=-1)
-    # cond_logprobs = torch.max(next_logprobs[:, cond_ids], dim=-1)[0]
-    cond_logprobs = torch.mean(next_logprobs[:, cond_ids], dim=-1)
+    if REDUCE == 'max':
+        cond_logprobs = torch.max(next_logprobs[:, cond_ids], dim=-1)[0]
+    elif REDUCE == 'mean':
+        cond_logprobs = torch.mean(next_logprobs[:, cond_ids], dim=-1)
     cond_logprobs /= (torch.max(cond_logprobs) - torch.min(cond_logprobs))
     cond_logprobs *= (torch.max(logprobs[:, ids_to_retain]) - torch.min(logprobs[:, ids_to_retain]))
 
@@ -56,21 +69,18 @@ COND_IDS = tokenizer.encode(COND)
 input_ids = torch.tensor([tokenizer.encode(PREFIX, add_special_tokens=True)]).to(DEV)
 
 for t in range(LENGTH):
-    print(t, '=' * 80)
+    # print(t, '=' * 80)
     with torch.no_grad():
-        print(tokenizer.decode(input_ids[0]))
-        print('-' * 80)
+        # print(tokenizer.decode(input_ids[0]))
+        # print('-' * 80)
         logits = model(input_ids)[0][:, -1]
         logits, ids_to_retain = top_k_filtering(logits, TOP_K)
-        for k in range(TOP_K):
-            print(tokenizer.decode([ids_to_retain[k]]), end=' ')
+        # for k in range(TOP_K):
+        #     print(tokenizer.decode([ids_to_retain[k]]), end=' ')
         logprobs = F.log_softmax(logits, dim=-1)
+        # r = np.random.randint(0, len(COND_IDS))
+        # probs = conditioning(logprobs, COND_IDS, model, input_ids, ids_to_retain)
         probs = conditioning(logprobs, COND_IDS, model, input_ids, ids_to_retain)
-        # r = np.random.randint(0, int(1.5*len(COND_IDS)))
-        # if r >= len(COND_IDS):
-        #     probs = torch.exp(logprobs)
-        # else:
-        #     probs = conditioning(logprobs, [COND_IDS[r]], model, input_ids, ids_to_retain)
         next_tokens = torch.multinomial(probs, num_samples=1)
         input_ids = torch.cat([input_ids, next_tokens], dim=-1)
 
